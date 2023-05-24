11ef0783868585df7749c8b6c27f739cdb504b56
Justin Dong <justindong@Justins-MacBook-Pro.local>
2023-05-24 13:35:41 -0400

    alligned html and javascript. Created containers for weather/history.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,32 @@
+// Api Url and key for weather
 var weatherApiUrl = 'https://api.openweathermap.org';
 var weatherApiKey = '2d9212370a96a1b044194ec9d251b1e2';
+var locationHistory = [];
 
+// Query Selects the important id
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
+var searchHistory = document.querySelector('#history');
 var todayContainer = document.querySelector('#today');
 var forecastContainer = document.querySelector('forecast');
-var searchHistory = document.querySelector('#history');
 
+// For search hisotry
 function searchHistory() {
     searchHistory.innerHTML = '';
+
+    for (var i = locationHistory.length -1; i >= 0; i++) {
+        var buttonEl = document.createElement('button');
+        buttonEl.setAttribute('type', 'button');
+
+        buttonEl.setAttribute('data-search', locationHistory[i]);
+        buttonEl.textContent = locationHistory[i];
+        searchHistory.append(buttonEl);
+    }
+}
+
+function appendSearchHistory(search) {
+    if (locationHistory.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistory.push(search);
 }
